fc5c3633ff9eff06e9ff711ec9b5eb3c4c78696f
ayushsingh165 <mail.ayushkumarsingh@gmail.com>
2025-03-23 06:36:07 -0700

    Create Stringpadhe.java

---
+++ Stringpadhe.java
@@ -0,0 +1,9 @@
+public class stingpadhe {
+    public static void main(String[] args) {
+        String firstname = "ayush";
+        String middlename = "kumar";
+        String lastname = "singh";
+        String result = firstname + " " + middlename +" "+ lastname;
+        System.out.println(result);
+    }
+}
